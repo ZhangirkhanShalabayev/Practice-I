b3eb2827053b4c2e1350975cc77b909ea5655ebf
Zhangrikhan <zhangirkhanshalabayev@gmail.com>
2026-02-03 16:32:32 +0500

    Add Practice1 Python basics examples

---
+++ Practice-1/Untitled-1.py
@@ -0,0 +1,6 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+print(type(x))
+print(type(y))
---
+++ Practice-1/casting.py
@@ -0,0 +1,12 @@
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a = "Hello"
+print(a)
+
+a = "Hello, World!"
+print(a[1])
+
+for x in "banana":
+  print(x)
